7cc50342d660cfb4261afe112c0fd220d305ec4e
Emma Davies <emmadavies11@live.co.uk>
2023-10-06 17:22:44 +0100

    import packages

---
+++ functions_sta.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+import spiceypy
+import os.path
+import glob
+import urllib.request
+from urllib.request import urlopen
+import json
+import astrospice
+from sunpy.coordinates import HeliocentricInertial, HeliographicStonyhurst
+from bs4 import BeautifulSoup
+import cdflib
+import pickle
+
+
