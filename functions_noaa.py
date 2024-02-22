81a1fbd4f542a50e90f20039c119512e0809f8ea
Emma Davies <emmadavies11@live.co.uk>
2024-02-22 13:12:24 +0100

    add notation and tidy up functions and paths

--- functions_noaa.py
+++ functions_noaa.py
@@ -3,11 +3,23 @@ import pandas as pd
 from datetime import datetime, timedelta
 from spacepy import pycdf
 import spiceypy
-# import os
-import glob
+import os
 import urllib.request
 import os.path
 import json
+
+
+"""
+NOAA/DSCOVR DATA PATH
+"""
+
+
+dscovr_path='/Volumes/External/data/dscovr/'
+
+
+"""
+DSCOVR BAD DATA FILTER
+"""
 
 
 def filter_bad_data(df, col, bad_val):
@@ -20,18 +32,29 @@ def filter_bad_data(df, col, bad_val):
     return df
 
 
+"""
+DSCOVR MAG and PLAS DATA
+# Can call MAG and PLAS last 7 days directly from https://services.swpc.noaa.gov/products/solar-wind/
+# If those files aren't working, can download manually from https://www.swpc.noaa.gov/products/real-time-solar-wind and load both using get_noaa_realtime_alt 
+# Raw data is in GSM coordinates; will implement transform to GSE/RTN
+"""
+
+
 def get_noaa_mag_realtime_7days():
     #mag data request produces file in GSM coords
     request_mag=urllib.request.urlopen('https://services.swpc.noaa.gov/products/solar-wind/mag-7-day.json')
     file_mag = request_mag.read()
     data_mag = json.loads(file_mag)
-    noaa_mag_gsm = pd.DataFrame(data_mag[1:], columns=['timestamp', 'b_x', 'b_y', 'b_z', 'lon_gsm', 'lat_gsm', 'b_tot'])
+    noaa_mag_gsm = pd.DataFrame(data_mag[1:], columns=['time', 'bx', 'by', 'bz', 'lon_gsm', 'lat_gsm', 'bt'])
 
-    noaa_mag_gsm['timestamp'] = pd.to_datetime(noaa_mag_gsm['timestamp'])
-    noaa_mag_gsm['b_x'] = noaa_mag_gsm['b_x'].astype('float')
-    noaa_mag_gsm['b_y'] = noaa_mag_gsm['b_y'].astype('float')
-    noaa_mag_gsm['b_z'] = noaa_mag_gsm['b_z'].astype('float')
-    noaa_mag_gsm['b_tot'] = noaa_mag_gsm['b_tot'].astype('float')
+    noaa_mag_gsm['time'] = pd.to_datetime(noaa_mag_gsm['time'])
+    noaa_mag_gsm['bx'] = noaa_mag_gsm['bx'].astype('float')
+    noaa_mag_gsm['by'] = noaa_mag_gsm['by'].astype('float')
+    noaa_mag_gsm['bz'] = noaa_mag_gsm['bz'].astype('float')
+    noaa_mag_gsm['bt'] = noaa_mag_gsm['bt'].astype('float')
+
+    noaa_mag_gsm.drop(columns = ['lon_gsm', 'lat_gsm'], inplace=True)
+
     return noaa_mag_gsm
 
 
@@ -40,25 +63,27 @@ def get_noaa_plas_realtime_7days():
     request_plas=urllib.request.urlopen('https://services.swpc.noaa.gov/products/solar-wind/plasma-7-day.json')
     file_plas = request_plas.read()
     data_plas = json.loads(file_plas)
-    noaa_plas = pd.DataFrame(data_plas[1:], columns=['timestamp', 'density', 'v_bulk', 'temperature'])
+    noaa_plas = pd.DataFrame(data_plas[1:], columns=['time', 'np', 'vt', 'tp'])
 
-    noaa_plas['timestamp'] = pd.to_datetime(noaa_plas['timestamp'])
-    noaa_plas['density'] = noaa_plas['density'].astype('float')
-    noaa_plas['v_bulk'] = noaa_plas['v_bulk'].astype('float')
-    noaa_plas['temperature'] = noaa_plas['temperature'].astype('float')
+    noaa_plas['time'] = pd.to_datetime(noaa_plas['time'])
+    noaa_plas['np'] = noaa_plas['np'].astype('float')
+    noaa_plas['vt'] = noaa_plas['vt'].astype('float')
+    noaa_plas['tp'] = noaa_plas['tp'].astype('float')
     return noaa_plas
 
 
-def get_noaa_realtime_alt(path = '/Volumes/External/Data/DSCOVR'):
+#Calling directly from json file: if json files are not working/ producing same data as seen on the realtime plots at https://www.swpc.noaa.gov/products/real-time-solar-wind:
+#download file manually, e.g. load 7 days data, 'Save as text', and load using 'get_noaa_realtime_alt()'
+def get_noaa_realtime_alt(path=f'{dscovr_path}'):
 
     filename = os.listdir(path)[0]
     noaa_alt = pd.read_table(f'{path}/{filename}', header=9, sep='\s+')
     noaa_alt = noaa_alt.reset_index()
-    noaa_alt['timestamp'] = pd.to_datetime(noaa_alt['index'] + ' ' + noaa_alt['Timestamp'], format='%Y-%m-%d %H:%M:%S')
+    noaa_alt['time'] = pd.to_datetime(noaa_alt['index'] + ' ' + noaa_alt['Timestamp'], format='%Y-%m-%d %H:%M:%S')
     noaa_alt = noaa_alt.drop(columns=['index', 'Timestamp'])
 
-    noaa_alt.rename(columns={'Bt-med': 'b_tot', 'Bx-med': 'b_x', 'By-med': 'b_y', 'Bz-med': 'b_z'}, inplace=True)
-    noaa_alt.rename(columns={'Dens-med': 'density', 'Speed-med': 'v_bulk', 'Temp-med': 'temperature'}, inplace=True)
+    noaa_alt.rename(columns={'Bt-med': 'bt', 'Bx-med': 'bx', 'By-med': 'by', 'Bz-med': 'bz'}, inplace=True)
+    noaa_alt.rename(columns={'Dens-med': 'np', 'Speed-med': 'vt', 'Temp-med': 'tp'}, inplace=True)
 
     noaa_alt.drop(columns = ['Source', 'Bt-min', 'Bt-max', 'Bx-min', 'Bx-max', 'By-min', 'By-max', 'Bz-min', 'Bz-max'], inplace=True)
     noaa_alt.drop(columns = ['Phi-mean', 'Phi-min', 'Phi-max', 'Theta-med', 'Theta-min', 'Theta-max'], inplace=True)
