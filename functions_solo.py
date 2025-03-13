fb18cc01ac3eb2726d675281e698612aba8fc580
Emma Davies <emmadavies11@live.co.uk>
2025-03-13 15:06:58 +0100

    add filter bad data for individual col

--- functions_solo.py
+++ functions_solo.py
@@ -24,13 +24,22 @@ SOLO BAD DATA FILTER
 """
 
 
-def filter_bad_data(df, col, bad_val):
+def filter_bad_data(df, col, bad_val): #filter across whole df
     if bad_val < 0:
         mask = df[col] < bad_val  # boolean mask for all bad values
     else:
         mask = df[col] > bad_val  # boolean mask for all bad values
     cols = [x for x in df.columns if x != 'timestamp']
     df.loc[mask, cols] = np.nan
+    return df
+
+
+def filter_bad_col(df, col, bad_val): #filter by individual columns
+    if bad_val < 0:
+        mask = df[col] < bad_val  # boolean mask for all bad values
+    else:
+        mask = df[col] > bad_val  # boolean mask for all bad values
+    df[col][mask] = np.nan
     return df
 
 
