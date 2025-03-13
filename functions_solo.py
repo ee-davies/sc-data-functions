580f2fd88d0dc568248cf470142c977b08ab2d48
Emma Davies <emmadavies11@live.co.uk>
2025-03-13 15:42:06 +0100

    add solo plas ll tp nan col

--- functions_solo.py
+++ functions_solo.py
@@ -329,6 +329,7 @@ def get_soloplas_ll(fp):
     df = filter_bad_col(df, 'vy', -1E30)
     df = filter_bad_col(df, 'vz', -1E30)
     df['vt'] = np.linalg.norm(df[['vx', 'vy', 'vz']], axis=1)
+    df['tp'] = np.nan
     return df
 
 
