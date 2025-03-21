baa39ea1d87ed8ff5b0d2693528f6cc486155ef9
Emma Davies <emmadavies11@live.co.uk>
2025-03-21 23:25:13 +0100

    add ulysses download mag function

--- functions_ulysses.py
+++ functions_ulysses.py
@@ -42,3 +42,32 @@ def filter_bad_col(df, col, bad_val): #filter by individual columns
     return df
 
 
+"""
+ULYSSES MAG DATA
+# obtained via https://cdaweb.gsfc.nasa.gov/pub/data/ulysses/mag_cdaweb/vhm_1min/
+# cdf files available in 1 min, 1 sec, m1
+"""
+
+
+#DOWNLOAD FUNCTIONS
+
+
+def download_ulyssesmag_1min(start_timestamp, end_timestamp, path=f'{ulysses_path}'+'mag/l2/1min'): 
+    start = start_timestamp.date()
+    end = end_timestamp.date() + timedelta(days=1)
+    while start < end:
+        year = start.year
+        date_str = f'{year}{start.month:02}{start.day:02}'
+        data_item_id = f'uy_1min_vhm_{date_str}_v01'
+        if os.path.isfile(f"{path}/{data_item_id}.cdf") == True:
+            print(f'{data_item_id}.cdf has already been downloaded.')
+            start += timedelta(days=1)
+        else:
+            try:
+                data_url = f'https://cdaweb.gsfc.nasa.gov/pub/data/ulysses/mag_cdaweb/vhm_1min/{year}/{data_item_id}.cdf'
+                urllib.request.urlretrieve(data_url, f"{path}/{data_item_id}.cdf")
+                print(f'Successfully downloaded {data_item_id}.cdf')
+                start += timedelta(days=1)
+            except Exception as e:
+                print('ERROR', e, data_item_id)
+                start += timedelta(days=1)
