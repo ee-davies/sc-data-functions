4fe62f429b7a2ca8320d301c0439e84c2018f2be
Emma Davies <emmadavies11@live.co.uk>
2025-03-21 15:55:53 +0100

    add ulysses bad data filters

--- functions_ulysses.py
+++ functions_ulysses.py
@@ -18,6 +18,27 @@ ulysses_path='/Volumes/External/data/ulysses/'
 kernels_path='/Volumes/External/data/kernels/'
 
 
+"""
+ULYSSES BAD DATA FILTER
+"""
 
+
+def filter_bad_data(df, col, bad_val): #filter across whole df
+    if bad_val < 0:
+        mask = df[col] < bad_val  # boolean mask for all bad values
+    else:
+        mask = df[col] > bad_val  # boolean mask for all bad values
+    cols = [x for x in df.columns if x != 'timestamp']
+    df.loc[mask, cols] = np.nan
+    return df
+
+
+def filter_bad_col(df, col, bad_val): #filter by individual columns
+    if bad_val < 0:
+        mask = df[col] < bad_val  # boolean mask for all bad values
+    else:
+        mask = df[col] > bad_val  # boolean mask for all bad values
+    df[col][mask] = np.nan
+    return df
 
 
