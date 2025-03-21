96723061efa5be467cd883e71b2cb9cc26c0cf3d
Emma Davies <emmadavies11@live.co.uk>
2025-03-21 15:55:22 +0100

    add ulysses functions preamble

---
+++ functions_ulysses.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+from spacepy import pycdf
+import cdflib
+import spiceypy
+import glob
+import urllib.request
+import os.path
+import pickle
+
+
+"""
+ULYSSES SERVER DATA PATH
+"""
+
+ulysses_path='/Volumes/External/data/ulysses/'
+kernels_path='/Volumes/External/data/kernels/'
+
+
+
+
+
