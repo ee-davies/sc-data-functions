9e37d0e8dd0e2cf5546a117b763c136676961543
Emma Davies <emmadavies11@live.co.uk>
2025-03-24 14:00:50 +0100

    add stereob paths and bad data updates

--- functions_stb.py
+++ functions_stb.py
@@ -15,13 +15,36 @@ import pickle
 from spacepy import pycdf
 
 
-def filter_bad_data(df, col, bad_val):
+"""
+STEREO-B DATA PATH
+"""
+
+
+stereob_path='/Volumes/External/data/stereob/'
+kernels_path='/Volumes/External/data/kernels/'
+
+
+"""
+STEREO-B BAD DATA FILTER
+"""
+
+
+def filter_bad_data(df, col, bad_val): #filter across whole df
     if bad_val < 0:
         mask = df[col] < bad_val  # boolean mask for all bad values
     else:
         mask = df[col] > bad_val  # boolean mask for all bad values
-    cols = [x for x in df.columns if x != 'Timestamp']
+    cols = [x for x in df.columns if x != 'timestamp']
     df.loc[mask, cols] = np.nan
+    return df
+
+
+def filter_bad_col(df, col, bad_val): #filter by individual columns
+    if bad_val < 0:
+        mask = df[col] < bad_val  # boolean mask for all bad values
+    else:
+        mask = df[col] > bad_val  # boolean mask for all bad values
+    df[col][mask] = np.nan
     return df
 
 
