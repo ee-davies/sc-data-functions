b89d79892f61a99b431ad5560d91584646c87a26
Emma Davies <emmadavies11@live.co.uk>
2025-03-24 11:12:17 +0100

    add ulysses pickle function

--- functions_ulysses.py
+++ functions_ulysses.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from spacepy import pycdf
 import cdflib
 import spiceypy
@@ -238,3 +238,95 @@ def get_ulysses_positions(time_series):
         positions.append(position)
     df_positions = pd.DataFrame(positions, columns=['time', 'x', 'y', 'z', 'r', 'lat', 'lon'])
     return df_positions
+
+
+"""
+OUTPUT COMBINED PICKLE FILE
+including MAG, PLAS, and POSITION data
+"""
+
+
+def create_ulysses_pkl(start_timestamp, end_timestamp=datetime.now(timezone.utc), level='l2', res='1min', output_path=ulysses_path):
+    
+    # #download solo mag and plasma data up to now 
+    # download_solomag_1min(start_timestamp)
+    # download_soloplas(start_timestamp)
+
+    #load in mag data to DataFrame and resample, create empty mag and resampled DataFrame if no data
+    # if empty, drop time column ready for concat
+    df_mag = get_ulyssesmag_range(start_timestamp, end_timestamp)
+    if df_mag is None:
+        print(f'Ulysses VHM/FGM data is empty for this timerange')
+        df_mag = pd.DataFrame({'time':[], 'bt':[], 'bx':[], 'by':[], 'bz':[]})
+        mag_rdf = df_mag.drop(columns=['time'])
+    else:
+        mag_rdf = df_mag.set_index('time').resample('1min').mean().reset_index(drop=False)
+        mag_rdf.set_index(pd.to_datetime(mag_rdf['time']), inplace=True)
+        
+    #load in plasma data to DataFrame and resample, create empty plasma and resampled DataFrame if no data
+    #only drop time column if MAG DataFrame is not empty
+    df_plas = get_ulyssesplas_range(start_timestamp, end_timestamp)
+    if df_plas is None:
+        print(f'Ulysses SWOOPS data is empty for this timerange')
+        df_plas = pd.DataFrame({'time':[], 'vt':[], 'vx':[], 'vy':[], 'vz':[], 'np':[], 'tp':[]})
+        plas_rdf = df_plas
+    else:
+        plas_rdf = df_plas.set_index('time').resample('1min').mean().reset_index(drop=False)
+        plas_rdf.set_index(pd.to_datetime(plas_rdf['time']), inplace=True)
+        if mag_rdf.shape[0] != 0:
+            plas_rdf = plas_rdf.drop(columns=['time'])
+
+    #need to combine mag and plasma dfs to get complete set of timestamps for position calculation
+    magplas_rdf = pd.concat([mag_rdf, plas_rdf], axis=1)
+    #some timestamps may be NaT so after joining, drop time column and reinstate from combined index col
+    magplas_rdf = magplas_rdf.drop(columns=['time'])
+    magplas_rdf['time'] = magplas_rdf.index
+     
+    #get solo positions for corresponding timestamps
+    ulysses_furnish()
+    ulysses_pos = get_ulysses_positions(magplas_rdf['time'])
+    ulysses_pos.set_index(pd.to_datetime(ulysses_pos['time']), inplace=True)
+    ulysses_pos = ulysses_pos.drop(columns=['time'])
+    spiceypy.kclear()
+
+    #produce final combined DataFrame with correct ordering of columns 
+    comb_df = pd.concat([magplas_rdf, ulysses_pos], axis=1)
+
+    #produce recarray with correct datatypes
+    time_stamps = comb_df['time']
+    dt_lst= [element.to_pydatetime() for element in list(time_stamps)] #extract timestamps in datetime.datetime format
+
+    ulysses=np.zeros(len(dt_lst),dtype=[('time',object),('bx', float),('by', float),('bz', float),('bt', float),\
+                ('vx', float),('vy', float),('vz', float),('vt', float),('np', float),('tp', float),\
+                ('x', float),('y', float),('z', float), ('r', float),('lat', float),('lon', float)])
+    ulysses = ulysses.view(np.recarray) 
+
+    ulysses.time=dt_lst
+    ulysses.bx=comb_df['bx']
+    ulysses.by=comb_df['by']
+    ulysses.bz=comb_df['bz']
+    ulysses.bt=comb_df['bt']
+    ulysses.vx=comb_df['vx']
+    ulysses.vy=comb_df['vy']
+    ulysses.vz=comb_df['vz']
+    ulysses.vt=comb_df['vt']
+    ulysses.np=comb_df['np']
+    ulysses.tp=comb_df['tp']
+    ulysses.x=comb_df['x']
+    ulysses.y=comb_df['y']
+    ulysses.z=comb_df['z']
+    ulysses.r=comb_df['r']
+    ulysses.lat=comb_df['lat']
+    ulysses.lon=comb_df['lon']
+    
+    #dump to pickle file
+    header='Ulysses L2 science data incl. magnetic field (VHM/FGM), plasma (SWOOPS), and heliospheric positions.' + \
+    'Timerange: '+ulysses.time[0].strftime("%Y-%b-%d %H:%M")+' to '+ulysses.time[-1].strftime("%Y-%b-%d %H:%M")+\
+    ', resampled to a time resolution of 1 min. '+\
+    'The data are available in a numpy recarray, fields can be accessed by ulysses.time, ulysses.bx, ulysses.vt, ulysses.r etc. '+\
+    'Total number of data points: '+str(ulysses.size)+'. '+\
+    'Units are btxyz [nT, RTN], heliospheric position x/y/z/r/lon/lat [AU, degree, HEEQ]. '+\
+    'Made with script by E.E. Davies (github @ee-davies, twitter @spacedavies). File creation date: '+\
+    datetime.now(timezone.utc).strftime("%Y-%b-%d %H:%M")+' UTC'
+
+    pickle.dump([ulysses,header], open(output_path+f'ulysses_rtn.p', "wb"))
