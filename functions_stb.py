8508de89d9ff60385dbea6309f295edf5f1ca1d0
Emma Davies <emmadavies11@live.co.uk>
2025-03-24 13:59:22 +0100

    add stereo b imports

--- functions_stb.py
+++ functions_stb.py
@@ -1,3 +1,20 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+import spiceypy
+import os.path
+import glob
+import urllib.request
+from urllib.request import urlopen
+import json
+import astrospice
+from sunpy.coordinates import HeliocentricInertial, HeliographicStonyhurst
+from bs4 import BeautifulSoup
+import cdflib
+import pickle
+from spacepy import pycdf
+
+
 def filter_bad_data(df, col, bad_val):
     if bad_val < 0:
         mask = df[col] < bad_val  # boolean mask for all bad values
