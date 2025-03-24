0d1ef9c857bede0efd407565aee4d216ae86b3b9
Emma Davies <emmadavies11@live.co.uk>
2025-03-24 11:26:55 +0100

    add planet position functions

--- functions_planets.py
+++ functions_planets.py
@@ -12,7 +12,7 @@ import pickle
 
 
 """
-PLANETS functions: positions of planets from spice kernels
+PLANETS functions: positions of planets from generic spice kernels
 """
 
 kernels_path='/Volumes/External/data/kernels/'
@@ -33,3 +33,32 @@ def generic_furnish():
     generic_kernels = os.listdir(generic_path)
     for kernel in generic_kernels:
         spiceypy.furnsh(os.path.join(generic_path, kernel))
+
+
+""""
+PLANET POSITIONS
+options include:
+        MERCURY BARYCENTER (1)  SATURN BARYCENTER (6)   MERCURY (199)
+        VENUS BARYCENTER (2)    URANUS BARYCENTER (7)   VENUS (299)
+        EARTH BARYCENTER (3)    NEPTUNE BARYCENTER (8)  MOON (301)
+        MARS BARYCENTER (4)     PLUTO BARYCENTER (9)    EARTH (399)
+        JUPITER BARYCENTER (5)  SUN (10)
+"""
+
+
+def get_planet_pos(t, planet): #doesn't automatically furnish, fix
+    if spiceypy.ktotal('ALL') < 1:
+        generic_furnish()
+    pos = spiceypy.spkpos(planet, spiceypy.datetime2et(t), "HEEQ", "NONE", "SUN")[0] #calls positions in HEEQ; can be changed
+    r, lat, lon = cart2sphere(pos[0],pos[1],pos[2])
+    position = t, pos[0], pos[1], pos[2], r, lat, lon
+    return position
+
+
+def get_planet_positions(time_series, planet):
+    positions = []
+    for t in time_series:
+        position = get_planet_pos(t, planet)
+        positions.append(position)
+    df_positions = pd.DataFrame(positions, columns=['time', 'x', 'y', 'z', 'r', 'lat', 'lon'])
+    return df_positions
