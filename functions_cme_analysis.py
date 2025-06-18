02479a57abfec4f73fc982c4e597861d2199990b
Emma Davies <emmadavies11@live.co.uk>
2025-06-18 15:36:35 +0200

    add DiP CME analysis function

--- functions_cme_analysis.py
+++ functions_cme_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
+from scipy import integrate
 
 """
 FUNCTIONS TO ANALYSE DIFFERENT PARTS OF ICMES
@@ -21,3 +22,16 @@ SHEATH
 """
 MAGNETIC EJECTA
 """
+
+def get_DiP(df, mo_start, mo_end):
+    mask = (df.time >= mo_start) & (df.time <= mo_end)
+    ME = df[mask]
+    times = [(time - np.min(ME['time'])).total_seconds() for time in ME['time']]
+    B_int = integrate.cumtrapz(ME['bt'], times)
+    #loop to find halfway point
+    i = 0
+    while B_int[i] < B_int[-1] / 2:
+        i += 1    
+    DiP = times[i]/times[-1]
+    return DiP
+
