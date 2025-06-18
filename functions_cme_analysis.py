b158cf2b48f885347edfb6ed610683b477882d72
Emma Davies <emmadavies11@live.co.uk>
2025-06-18 15:24:53 +0200

    add new functions for cme event analysis

---
+++ functions_cme_analysis.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+
+"""
+FUNCTIONS TO ANALYSE DIFFERENT PARTS OF ICMES
+"""
+
+"""
+GENERAL
+"""
+
+"""
+SHOCK
+"""
+
+"""
+SHEATH
+"""
+
+"""
+MAGNETIC EJECTA
+"""
