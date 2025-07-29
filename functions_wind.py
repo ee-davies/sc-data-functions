af4d23dad32486a5233c67070b4b439b6924e328
Emma Davies <emmadavies11@live.co.uk>
2025-07-29 11:42:17 +0200

    add wind swe rtn capability to convert gse to rtn when original rtn files are missing

--- functions_wind.py
+++ functions_wind.py
@@ -458,7 +458,7 @@ def get_windswe_rtn(fp): #choice between nonlin or moments
     return df
 
 
-def get_windswe_rtn_range(start_timestamp, end_timestamp, path=wind_path+'swe'):
+def get_windswe_rtn_range(start_timestamp, end_timestamp, path=wind_path):
     """Pass two datetime objects and grab .cdf files between dates, from
     directory given."""
     df = None
@@ -467,7 +467,7 @@ def get_windswe_rtn_range(start_timestamp, end_timestamp, path=wind_path+'swe'):
     while start <= end:
         date_str = f'{start.year}{start.month:02}{start.day:02}'
         try:
-            fn = glob.glob(f'{path}/rtn/wi_h1_swe_rtn_{date_str}*')[0]
+            fn = glob.glob(f'{path}/swe/rtn/wi_h1_swe_rtn_{date_str}*')[0]
             _df = get_windswe_rtn(fn)
             if _df is not None:
                 if df is None:
@@ -477,8 +477,11 @@ def get_windswe_rtn_range(start_timestamp, end_timestamp, path=wind_path+'swe'):
         except Exception as e:
             print('ERROR:', e, f'wi_h1_swe_rtn_{date_str} does not exist, converting from wi_h1_swe_{date_str} (GSE) instead')
             try:
-                fn = glob.glob(f'{path}/h1/wi_h1_swe_{date_str}*')[0]
-                _df = get_windswe_gse(fn)
+                fn_swe = glob.glob(f'{path}/swe/h1/wi_h1_swe_{date_str}*')[0]
+                _df_swe = get_windswe_gse(fn_swe)
+                fn_pos = glob.glob(f'{path}/orbit/wi_or_pre_{date_str}*')[0]
+                _df_pos = get_wind_pos(fn_pos, 'GSE')
+                _df = windswe_gse_to_rtn(_df_swe, _df_pos)
                 if _df is not None:
                     if df is None:
                         df = _df.copy(deep=True)
