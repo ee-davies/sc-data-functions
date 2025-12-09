67950f2c185855a2b690e7f2d03ce3c9b4496a4c
Emma Davies <emmadavies11@live.co.uk>
2025-12-09 16:07:53 +0100

    add old functions that need editing

--- functions_cme_analysis.py
+++ functions_cme_analysis.py
@@ -79,3 +79,59 @@ def slope_fitting(df, start_time, end_time):
     slope, intercept, r_value, p_value, std_err = stats.linregress(x[mask], y[mask])
     line = np.transpose(slope*x+intercept)
     return times, line, slope, intercept, r_value, p_value, std_err
+
+
+## OLD FUNCTIONS, NEED UPDATING
+
+def make_icme_v_profile(icme_start, mo_start, mo_end, resampled_df):
+    #fit velocity profile within mo 
+    mo_fit = slope_fitting(resampled_df, mo_start, mo_end)
+    mo_df = pd.DataFrame(mo_fit[0])
+    mo_df['v_bulk'] = mo_fit[1]
+    #create sheath df
+    sheath_mask = (resampled_df['Timestamp']>= icme_start) & (resampled_df['Timestamp']< mo_start)
+    sheath_df = resampled_df[sheath_mask]
+    if mo_df['v_bulk'][0] > sheath_df['v_bulk'].mean():
+        sheath_df = resampled_df[sheath_mask].assign(v_bulk=mo_df['v_bulk'][0])
+    else:
+        sheath_df = resampled_df[sheath_mask].assign(v_bulk=sheath_df['v_bulk'].mean()) #calculate mean of sheath and replace v_bulk column
+    icme_df = pd.concat([sheath_df, mo_df])
+    #drop position columns (to not get confused)
+    icme_df = icme_df.drop(columns=['X', 'Y', 'Z'])
+    return icme_df
+
+
+def calc_time_delta(resampled_df, icme_df):
+    #drop old v_bulk column from resampled df 
+    resampled_df = resampled_df.drop(columns=['v_bulk'])
+    #merge with new velocity profile, which also produces just values within ICME
+    merge=pd.merge(icme_df, resampled_df, how='inner', left_index=True, right_index=True)
+    #add columns with new variables needed for time_delta calculation and calculate
+    merge['Ve'] = 30
+    merge['W'] = np.tan(0.5*np.arctan(merge['v_bulk']/428))
+    merge['Delta_t'] = (merge['X']/merge['v_bulk']) * (1 + ((merge['Y']*merge['W'])/merge['X']))/(1 - merge['Ve']*merge['W']/merge['v_bulk'])
+    #make new df with just time delta and icme timestamps as index
+    t_df = pd.DataFrame(merge['Delta_t'])
+    return t_df
+
+
+def apply_time_delta(df):
+    t = []
+    for i in range(len(df)):
+        new_t = df['Timestamp'].iloc[i] + timedelta(seconds=df['Delta_t'].iloc[i])
+        t = np.append(t, new_t)
+    df['New_Timestamp'] = t
+    return df
+
+
+def time_shift_df(df, t_df, resample_min):
+    #resample df and set timestamp as index
+    rdf = df.set_index('Timestamp').resample(f'{resample_min}min').mean().reset_index(drop=False)
+    rdf.set_index(pd.to_datetime(rdf['Timestamp']), inplace=True)
+    #concatenate df and t_df 
+    shifted_rdf = pd.concat([rdf, t_df], axis=1)
+    #back and forward fill t_df values 
+    shifted_rdf['Delta_t'] = shifted_rdf['Delta_t'].ffill().bfill()
+    #apply time_delta to each timestamp
+    final_df = apply_time_delta(shifted_rdf)
+    return final_df
