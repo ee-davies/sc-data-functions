7a277961cec639c7df3c816a8b74b7e4f09e9211
Emma Davies <emmadavies11@live.co.uk>
2026-03-04 10:37:27 +0100

    update bad data filter column functions to remove slice error for solo and imap

--- functions_imap.py
+++ functions_imap.py
@@ -44,10 +44,10 @@ def filter_bad_data(df, col, bad_val): #filter across whole df
 
 def filter_bad_col(df, col, bad_val): #filter by individual columns
     if bad_val < 0:
-        mask = df[col] < bad_val  # boolean mask for all bad values
+        mask_vals = df[col] < bad_val  # boolean mask for all bad values
     else:
-        mask = df[col] > bad_val  # boolean mask for all bad values
-    df[col][mask] = np.nan
+        mask_vals = df[col] > bad_val  # boolean mask for all bad values
+    df[col].mask(mask_vals)
     return df
 
 
